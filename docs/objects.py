9edb92d61b5f840925c0e09382dd4f01baa9fa5f
JoFrhwld <JoFrhwld@gmail.com>
2024-05-18 12:22:44 -0400

    docs fix

--- docs/objects.py
+++ docs/objects.py
@@ -1,4 +1,4 @@
-import new_fave
+import fave_measurement_point
 import sphobjinv as soi
 from pathlib import Path
 inv = soi.Inventory(fname_plain = Path("objects.txt"))
@@ -6,10 +6,10 @@ inv = soi.Inventory(fname_plain = Path("objects.txt"))
 for idx in range(len(inv.objects)):
     obj = inv.objects[idx]
     basename = obj.name.split(".")[-1]
-    if hasattr(new_fave, basename):
+    if hasattr(fave_measurement_point, basename):
         obj_dict = obj.json_dict()
         if not "new_fave." + basename in obj_dict:
-            obj_dict["name"] = "aligned_textgrid." + basename
+            obj_dict["name"] = "fave_measurement_point." + basename
             new_obj = soi.DataObjStr(**obj_dict)
             inv.objects.append(new_obj)
             
